d027056e2a814ecec10fe3631a0432d52fc177cd
Chaiyapruek Muangsiri <cmp.poon@gmail.com>
2024-03-02 21:19:34 +0800

    finish components

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,11 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        "primary-green": "hsl(75, 94%, 57%)",
+        "primary-gray": "hsl(0, 0%, 20%)",
+        "dark-gray": "hsl(0, 0%, 12%)",
+        "off-black": "hsl(0, 0%, 8%)",
       },
     },
   },
